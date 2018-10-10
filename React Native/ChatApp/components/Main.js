011e8bc03aab2e2c579b191cc47f34ee7ab62611
Felipe Valencia <>
2018-10-10 17:28:57 -0500

    Learning React Native

--- React Native/ChatApp/components/Main.js
+++ React Native/ChatApp/components/Main.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import {
+    StyleSheet,
+    Text, // <- Add Text
+    TextInput,
+    TouchableOpacity, // <- Add TouchableOpacity
+    View,
+} from 'react-native';
 
 class Main extends Component {
+    state = {name: " "};
+    onPress = () => {
+        this.props.navigate("Chat",{name: this.state.name})
+    }
+    onChangeText = name => this.setState({ name });
   render() {
-    return <View />;
+    return (
+        <View>
+            <Text style={styles.title}>Enter your name:</Text>
+            <TextInput
+                onChangeText={this.onChangeText}
+                style={styles.nameInput}
+                placeHolder="John Cena"
+                value={this.state.name}
+            />
+            <TouchableOpacity onPress={this.onPress}>
+                <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
+        </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({});
+const offset = 24;
 
+const styles = StyleSheet.create({
+    nameInput: {
+        height: offset * 2,
+        margin: offset,
+        paddingHorizontal: offset,
+        borderWidth: 1,
+    },
+    title: {
+        marginTop: offset,
+        marginLeft: offset,
+        fontSize: offset,
+    },
+    buttonText: {
+        marginLeft: offset,
+        fontSize: offset,
+    },
+})
 export default Main;
