1a3154925536d8f2664e46e6fe7a001b2ef470d0
Felipe Valencia <>
2018-10-10 15:43:46 -0500

    React Native ChatApp

---
+++ React Native/ChatApp/App.js
@@ -0,0 +1,8 @@
+import Main from "./components/Main"
+import Chat from "./components/Chat"
+import { createStackNavigator } from "react-navigation"
+const navigator = createStackNavigator({
+    Main: {screen:  Main},
+    Chat: {screen: Chat}
+});
+export default navigator
