beac9327b47613f89af3eaae357e955bb05bee64
Felipe Valencia <fvalencia@vc-soft.com>
2019-02-02 13:19:49 -0500

    Android Tutorial

---
+++ Android/HelloWorld/app/src/main/java/valencia/android/helloworld/DisplayMessageActivity.java
@@ -0,0 +1,20 @@
+package valencia.android.helloworld;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        String reverse = new StringBuilder(message).reverse().toString();
+        textView.setText(reverse);
+    }
+}
